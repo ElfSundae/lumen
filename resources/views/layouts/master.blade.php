edd7abfac52ef472138c07f495c0fc938fd2d555
Elf Sundae <elf.sundae@gmail.com>
2017-05-31 07:28:45 +0800

    Add description and keywords to the master view

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -4,6 +4,8 @@
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
   @isset($title)<title>{{ $title }}</title>@endisset
+  @isset($description)<meta name="description" content="{{ $description }}">@endisset
+  @isset($keywords)<meta name="keywords" content="{{ $keywords }}">@endisset
   <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
   <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico">
   <!--[if lte IE 9]><script src="//cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.min.js"></script><![endif]-->
