03cc713e43d0e5bf2e3cc688c775b9992123c1db
Elf Sundae <elf.sundae@gmail.com>
2017-06-04 21:35:10 +0800

    minor

--- resources/views/partials/google-analytics.blade.php
+++ resources/views/partials/google-analytics.blade.php
@@ -1,11 +1,11 @@
-@if (config('services.google_analytics.id'))
+@if ($id = config('services.google_analytics.id'))
 <script>
   setTimeout(function() {
     (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
       (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
       m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
     })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-    ga('create', "{{ config('services.google_analytics.id') }}", "{{ config('services.google_analytics.cookie_domain') ?: 'auto' }}", {'allowLinker': true});
+    ga('create', "{{ $id }}", "{{ config('services.google_analytics.cookie_domain', 'auto') }}", {'allowLinker': true});
     ga('require', 'linker');
     ga('send', 'pageview');
     if (typeof setupGA === "function") setupGA(ga);
