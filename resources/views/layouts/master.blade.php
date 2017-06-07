eaac697c45576adaa4edc55a7624b2636564dc61
Elf Sundae <elf.sundae@gmail.com>
2017-06-08 00:44:27 +0800

    Use auto scheme

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -12,7 +12,7 @@
   @stack('css')
   <!--[if lt IE 9]>
   <script src="//cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.min.js"></script>
-  <script src=https://cdnjs.cloudflare.com/ajax/libs/respond.js/1.4.2/respond.min.js></script>
+  <script src=//cdnjs.cloudflare.com/ajax/libs/respond.js/1.4.2/respond.min.js></script>
   <![endif]-->
   @include('partials.google-analytics')
 </head>
