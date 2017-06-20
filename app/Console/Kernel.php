2f3084ad025e92cddf34cb50884cdf477e7956c7
Elf Sundae <elf.sundae@gmail.com>
2017-06-20 21:24:49 +0800

    Configure "app" for console

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Console;
 
+use Laravel\Lumen\Application;
 use Illuminate\Console\Scheduling\Schedule;
 use Laravel\Lumen\Console\Kernel as ConsoleKernel;
 
@@ -15,6 +16,19 @@ class Kernel extends ConsoleKernel
     protected $commands = [
         Commands\KeyGenerateCommand::class,
     ];
+
+    /**
+     * Create a new console kernel instance.
+     *
+     * @param  \Laravel\Lumen\Application  $app
+     * @return void
+     */
+    public function __construct(Application $app)
+    {
+        parent::__construct($app);
+
+        $this->app->configure('app');
+    }
 
     /**
      * Define the application's command schedule.
